e1a157d84cc292fa53cab6d1af329a47691e721c
Vaughn McPherson <Vaughnsmcp@gmail.com>
2020-04-27 15:06:52 -0700

    header

---
+++ pen-pals/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Header extends React.Component {
+
+    render (){
+
+        return(<h1 className ="jumbotron"> Welcome to Pen Pals!!!!!!!!!!!!!!</h1>)
+    };
+} export default Header; 
+
