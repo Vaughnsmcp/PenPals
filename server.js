a4c7b5ed30e1aea29e073f0dd0cf205c5555b0de
Spencer Fife <scottyfife17@gmail.com>
2020-05-03 17:15:27 -0700

    added api crud, edited schema

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require(`express`);
 const path = require(`path`);
+const mongojs = require(`mongojs`);
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -13,8 +14,79 @@ if (process.env.NODE_ENV === `production`) {
   app.use(express.static(`client/build`));
 }
 
+const dbName = `penpals`;
+const collections = [`userinfo`];
+const db = mongojs(dbName, collections);
+
+db.on(`error`, (error) => {
+  console.log(`database error: `, error);
+});
+
 // Define API routes here
 
+app.post(`/submit`, (req, res) => {
+  console.log(req.body);
+  db.userinfo.insertOne(req.body, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+app.get(`/find/:id`, (req, res) => {
+  db.userinfo.findOne(
+    {
+      _id: mongojs.objectId(req.params.id),
+    },
+    (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    }
+  );
+});
+
+app.post(`/update/:id`, (req, res) => {
+  db.userinfo.updateOne(
+    {
+      _id: mongojs.objectId(req.params.id),
+    },
+    {
+      $set: {
+        name: req.body.name,
+        info: req.body.info,
+        link: req.body.link,
+        inquiry: req.body.inquiry,
+      },
+    },
+    (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    }
+  );
+});
+
+app.delete(`/delete/:id`, (req, res) => {
+  db.userinfo.remove(
+    {
+      _id: mongojs.objectId(req.params.id),
+    },
+    (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    }
+  );
+});
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get(`*`, (req, res) => {
