f5e6c66a0009b96301f309d79e79dc1e8b01de16
Spencer Fife <scottyfife17@gmail.com>
2020-05-03 18:35:54 -0700

    created utils directory and started API.js

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -3,18 +3,25 @@ import Wrapper from "../components/Wrapper";
 import Header from "../components/Header";
 
 function Portfolio() {
-    return (
-        <>
-            <Header message="Build your showcase!" />
-            <Wrapper>
-                <form>
-                    <div class="form-group">
-                        <label for="exampleFormControlFile1">Add your beautiful face!</label>
-                        <input type="file" class="form-control-file" id="exampleFormControlFile1"></input>
-                    </div>
-                </form>
-            </Wrapper>
-        </>
-    );
-} 
+  return (
+    <>
+      <Header message="Build your showcase!" />
+      <Wrapper>
+        <form>
+          <div class="form-group">
+            <label for="exampleFormControlFile1">
+              Add your beautiful face!
+            </label>
+            <input
+              type="file"
+              class="form-control-file"
+              id="exampleFormControlFile1"
+            ></input>
+          </div>
+        </form>
+      </Wrapper>
+    </>
+  );
+}
 export default Portfolio;
+//need to add more form functionality
